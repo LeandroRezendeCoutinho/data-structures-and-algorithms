8de7e8292b365f9518c622417e94d0ca113f9ffb
leandrorezendecoutinho <leandro.rc.contato@gmail.com>
2022-02-07 08:02:21 -0300

    stack with symbol to simulate private items

--- src/stack-object.js
+++ src/stack-object.js
@@ -1,11 +1,13 @@
+const _items = Symbol('stackItems')
+
 class Stack {
   constructor() {
     this.count = 0
-    this.items = {}
+    this[_items] = []
   }
 
   push(element) {
-    this.items[this.count] = element
+    this[_items][this.count] = element
     this.count++
   }
 
@@ -22,8 +24,8 @@ class Stack {
       return undefined
     }
     this.count--
-    const result = this.items[this.count]
-    delete this.items[this.count]
+    const result = this[_items][this.count]
+    delete this[_items][this.count]
     return result
   }
 
@@ -32,26 +34,39 @@ class Stack {
       return undefined
     }
 
-    return this.items[this.count - 1]
+    return this[_items][this.count - 1]
   }
 
   clear() {
     this.count = 0
-    this.items = {}
+    this[_items] = {}
   }
 
   toString() {
     if (this.isEmpty()) {
       return ''
     }
-    let objString = `${this.items[0]}`
+    let objString = `${this[_items][0]}`
     for (let i = 1; i < this.count; i++) {
-      objString = `${objString}, ${this.items[i]} `
+      objString = `${objString}, ${this[_items][i]} `
     }
     return objString
   }
 }
 
-const stack = new Stack()
+let stack = new Stack()
 stack.push(5)
 stack.push(8)
+console.log(stack.toString())
+
+stack = new Stack()
+console.log(Object.getOwnPropertyNames(stack))
+console.log(Object.keys(stack))
+console.log(stack.items)
+
+let objectSymbol = Object.getOwnPropertySymbols(stack)
+console.log(objectSymbol.length)
+console.log(objectSymbol)
+console.log(objectSymbol[0])
+stack[objectSymbol[0]].push(1)
+stack.toString()
